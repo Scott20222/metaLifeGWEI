4d75602eb480c6bad41b82d5b1d32dfc5b2ebc45
ubltroll <chen1045bc@vip.qq.com>
2022-07-08 14:56:41 +0800

    cors changes

--- GWEI_service/server.py
+++ GWEI_service/server.py
@@ -9,7 +9,7 @@ from data_exporter import export_data, EXPORTS_FOLDER
 app = Flask(__name__)
 app.secret_key = "yibor!!!"
 
-CORS(app)
+CORS(app, supports_credentials=True)
 
 def api_response(data, errorCode = 0):
     return jsonify({'success': errorCode == 0, 'msg': data, 'code': errorCode, 'timestamp': int(time.time())})
